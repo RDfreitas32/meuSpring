14fd7df25b8f127a9a337902a74141395c1a7e27
RodrigoF32 <rodrigofreitas3223@outlook.com>
2023-11-13 23:05:17 -0300

    Service Layer, compenent registration

--- src/main/java/com/springproject/demo/config/TestConfig.java
+++ src/main/java/com/springproject/demo/config/TestConfig.java
@@ -16,17 +16,24 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired
 	private UserRepository userRepository;
-	// Para executar automaticamente a interface CommandLineRunner
+	
 	@Override
 	public void run(String... args) throws Exception {
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456"); 
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456"); 
 		
-		// Inserindo os dois objetos no banco de dados através do userRepository
-		//passando uma lista de objetos o saveAll salva essa lista no BD
+
 		userRepository.saveAll(Arrays.asList(u1, u2));
 
 	}
+	// CommandLineRunner me traz o void, me permite executar a geração e inserção desses objtos no bd ao iniciar o programa
+
+	//Autowired é uma notation para me permitir instanciar um tipo User em TestConfig
+	//me permite gerar essa dependência, que é instanciar um obj de outro tipo em uma classe qualquer
 	
+	// UserRepository - Inserindo os dois objetos no banco de dados através do userRepository
+	//passando uma lista de objetos o saveAll salva essa lista no BD
+	
+	// void run - Para executar automaticamente a interface CommandLineRunner
 	
 }
---
+++ src/main/java/com/springproject/demo/services/UserService.java
@@ -0,0 +1,29 @@
+package com.springproject.demo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.springproject.demo.entities.User;
+import com.springproject.demo.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+}
+
+// @Autowired me permite instanciar um tipo UserRepository dentro de UserService, fazendo a injeção de dependêmcia
