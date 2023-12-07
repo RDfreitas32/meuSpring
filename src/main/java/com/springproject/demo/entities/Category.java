3619c5ec42cc5012996fc5538258023f763c1449
RodrigoF32 <rodrigofreitas3223@outlook.com>
2023-12-07 18:06:28 -0300

    many-to-many association with JoinTable

--- src/main/java/com/springproject/demo/entities/Category.java
+++ src/main/java/com/springproject/demo/entities/Category.java
@@ -1,12 +1,15 @@
 package com.springproject.demo.entities;
 
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -18,6 +21,9 @@ public class Category implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+
+	@ManyToMany(mappedBy = "categories")
+	private Set<Product> products = new HashSet<>();
 	
 	public Category() {
 		
---
+++ src/main/java/com/springproject/demo/services/ProductService.java
@@ -0,0 +1,29 @@
+package com.springproject.demo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.springproject.demo.entities.Product;
+import com.springproject.demo.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository repository;
+	
+	public List<Product> findAll() {
+		return repository.findAll();
+	}
+	
+	public Product findById(Long id) {
+		Optional<Product> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+}
+
+// @Autowired me permite instanciar um tipo ProductRepository dentro de ProductService, fazendo a injeção de dependêmcia
